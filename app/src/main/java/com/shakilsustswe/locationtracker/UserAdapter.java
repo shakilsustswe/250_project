70592aa988c600e81bbf1d427d4151b15932f49c
fahim <mdfahim1194@gmail.com>
2021-07-01 04:17:46 +0600

    tmi akhon r kisu koro na,, amr ta dekhoo,, just XML add korlm,, android android studio te configuration problem solved hoye,, actual code korboo

--- app/src/main/java/com/shakilsustswe/locationtracker/UserAdapter.java
+++ app/src/main/java/com/shakilsustswe/locationtracker/UserAdapter.java
@@ -1,16 +1,13 @@
 package com.shakilsustswe.locationtracker;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
 
