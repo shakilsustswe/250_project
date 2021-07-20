4ea1203424d8c51049abacd04a4c0035eca4fa75
fahim <mdfahim1194@gmail.com>
2021-07-20 20:45:58 +0600

    Working on User_location java file & fragment_user_location.xml adding user current location & searching feature

--- app/src/main/java/com/shakilsustswe/locationtracker/User_location.java
+++ app/src/main/java/com/shakilsustswe/locationtracker/User_location.java
@@ -7,14 +7,22 @@ import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -24,6 +32,7 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
@@ -33,6 +42,12 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
 public class User_location extends Fragment implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
@@ -40,10 +55,18 @@ public class User_location extends Fragment implements
 
     private GoogleMap mMap;
     private GoogleApiClient googleApiClient;
-    private static final int Request_User_Location_Code = 99;
+    private static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     private Location lastLocation;
     private Marker currentUserLocationMarker;
     private LocationRequest locationRequest;
+    private LocationManager locationManager;
+    private String provider;
+
+    private EditText addressField;
+    private ImageButton searchingBtn;
+    private ImageButton secDirection;
+
+    GoogleMapOptions options = new GoogleMapOptions();
 
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
@@ -53,12 +76,23 @@ public class User_location extends Fragment implements
             if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
-            googleMap.setMyLocationEnabled(true);
 
-            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(false);
             mMap = googleMap;
-            if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-            {
+            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+            mMap.getUiSettings().setZoomGesturesEnabled(true);
+            mMap.getUiSettings().setCompassEnabled(true);
+
+            //Initialize Google Play Services
+            if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(getContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    buildGoogleApiClient();
+                    mMap.setMyLocationEnabled(true);
+                }
+            } else {
                 buildGoogleApiClient();
                 mMap.setMyLocationEnabled(true);
             }
@@ -68,7 +102,7 @@ public class User_location extends Fragment implements
     protected synchronized void buildGoogleApiClient() {
         googleApiClient = new GoogleApiClient.Builder(getContext())
                 .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this::onConnectionFailed)
+                .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
         googleApiClient.connect();
@@ -79,7 +113,21 @@ public class User_location extends Fragment implements
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_user_location, container, false);
+        View v =  inflater.inflate(R.layout.fragment_user_location, container, false);
+        addressField = v.findViewById(R.id.fragment_user_location_searchingEditTxt);
+        searchingBtn = v.findViewById(R.id.fragment_user_location_searchingBtn);
+        secDirection = v.findViewById(R.id.fragment_user_locationDirectionBtn);
+
+        searchingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showSearchingLocation();
+            }
+        });
+
+
+
+        return v;
     }
 
     @Override
@@ -88,8 +136,9 @@ public class User_location extends Fragment implements
 
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            checkUserLocationPermission();
+            checkLocationPermission();
         }
+
 
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
@@ -98,57 +147,79 @@ public class User_location extends Fragment implements
         }
     }
 
-    private boolean checkUserLocationPermission() {
-        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)){
-                ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location_Code);
-            }
-            else{
-                ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, Request_User_Location_Code);
-            }
-
-            return false;
-        }
-        else{
-            return true;
-        }
-    }
 
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
         lastLocation = location;
-
         if(currentUserLocationMarker != null){
             currentUserLocationMarker.remove();
         }
 
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
-        markerOptions.title("Your Current Location");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
 
-        currentUserLocationMarker = mMap.addMarker(markerOptions);
+        try{
+            locationManager = (LocationManager)
+                    getContext().getSystemService(Context.LOCATION_SERVICE);
+            provider = locationManager.getBestProvider(new Criteria(), true);
+            if (ActivityCompat.checkSelfPermission(getContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                            != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            Location locations = locationManager.getLastKnownLocation(provider);
+            List<String> providerList = locationManager.getAllProviders();
+            if (null != locations && null != providerList && providerList.size() > 0) {
+                double longitude = locations.getLongitude();
+                double latitude = locations.getLatitude();
+                Geocoder geocoder = new Geocoder(getContext(),
+                        Locale.getDefault());
+                try {
+                    List<Address> listAddresses = geocoder.getFromLocation(latitude,
+                            longitude, 1);
+                    if (null != listAddresses && listAddresses.size() > 0) {
+                        String state = listAddresses.get(0).getAdminArea();
+                        String country = listAddresses.get(0).getCountryName();
+                        String subLocality = listAddresses.get(0).getSubLocality();
+                        markerOptions.title("" + latLng + "," + subLocality + "," + state
+                                + "," + country);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            currentUserLocationMarker = mMap.addMarker(markerOptions);
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomBy(16));
-
-        if(googleApiClient != null){
-            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            if (googleApiClient != null) {
+                LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient,
+                        this);
+            }
         }
+        catch (Exception e){
+
+        }
+
+
     }
 
     @Override
     public void onConnected(@Nullable @org.jetbrains.annotations.Nullable Bundle bundle) {
-        locationRequest = new LocationRequest();
-        locationRequest.setInterval(1100);
-        locationRequest.setFastestInterval(1100);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        if(ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this::onLocationChanged);
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,
+                    locationRequest, this);
         }
     }
 
@@ -167,18 +238,97 @@ public class User_location extends Fragment implements
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         switch (requestCode) {
-            case Request_User_Location_Code:
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ContextCompat.checkSelfPermission(getContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
                         if (googleApiClient == null) {
                             buildGoogleApiClient();
                         }
                         mMap.setMyLocationEnabled(true);
                     }
                 } else {
-                    Toast.makeText(getContext(), "Permission Denied...", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getContext(), "permission denied",
+                            Toast.LENGTH_LONG).show();
                 }
                 return;
+            }
         }
     }
+
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            } else {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+
+
+
+    private void showSearchingLocation() {
+
+        String address = addressField.getText().toString();
+
+        List<Address> addressList = null;
+        MarkerOptions userMarkerOptions = new MarkerOptions();
+        if(!TextUtils.isEmpty(address)){
+            secDirection.setVisibility(View.VISIBLE);
+            Toast.makeText(getContext(), "Tap to conner see the direction road map", Toast.LENGTH_SHORT).show();
+            Geocoder geocoder = new Geocoder(getContext());
+            try{
+                addressList = geocoder.getFromLocationName(address, 6);
+                if(addressList != null){
+                    for(int i = 0; i < addressList.size(); i++){
+                        Address userAddress = addressList.get(i);
+
+                        LatLng latLng = new LatLng(userAddress.getLatitude(), userAddress.getLongitude());
+
+                        userMarkerOptions.position(latLng);
+                        userMarkerOptions.title(address);
+                        userMarkerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+                        mMap.clear();
+                        mMap.addMarker(userMarkerOptions);
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
+                    }
+                }
+                else{
+                    Toast.makeText(getContext(),  "Location not found....", Toast.LENGTH_SHORT).show();
+                }
+            }
+            catch (Exception e){
+
+            }
+        }
+        else{
+            if(TextUtils.isEmpty(addressField.getText().toString())){
+                secDirection.setVisibility(View.INVISIBLE);
+            }
+            Toast.makeText(getContext(), "Please enter a location name", Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+
+
 }
